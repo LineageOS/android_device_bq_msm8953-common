2973fe7346197a15a3e3b33e30bfdb25c58e06cd
Brinly Taylor <brinly@brinly.me>
2017-07-30 08:48:32 +1000

    bardock: use USE_SENSOR_MULTI_HAL flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,9 @@ BOARD_USES_QCOM_HARDWARE := true
 # RIL
 TARGET_RIL_VARIANT := caf
 
+# Sensor
+USE_SENSOR_MULTI_HAL := true
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
