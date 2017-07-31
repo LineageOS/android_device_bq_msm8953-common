38cf3de9b40a77ca302e2a6a58dc4e8f4ab17a01
Brinly Taylor <brinly@brinly.me>
2017-07-31 18:33:57 +1000

    bardock: Build lights HAL

--- device.mk
+++ device.mk
@@ -145,6 +145,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8953
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert
