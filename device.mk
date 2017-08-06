9e5abb1b9ebe46568b06ae5ea7e13789382fe315
Brinly T <brinly@brinly.me>
2017-08-07 05:25:40 +1000

    bardock: device.mk: Cleanup copy of config files
    * Minor cleanups, no real work done here

--- device.mk
+++ device.mk
@@ -75,7 +75,6 @@ PRODUCT_PACKAGES += \
     tinymix
 
 ## Audio configuration files
-
 PRODUCT_COPY_FILES += \
     hardware/qcom/audio-caf/msm8937/configs/msm8953/audio_policy.conf:system/etc/audio_policy.conf \
     hardware/qcom/audio-caf/msm8937/configs/msm8953/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
@@ -103,9 +102,7 @@ PRODUCT_COPY_FILES += \
 
 #XML Audio configuration files
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom/audio-caf/msm8937/configs/msm8953/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
-
-PRODUCT_COPY_FILES += \
+    $(TOPDIR)hardware/qcom/audio-caf/msm8937/configs/msm8953/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
