0bee80c82df3988becbe11a37b9aebbfff25c45f
Brinly T <brinly@brinly.me>
2017-08-16 17:36:32 +1000

    bardock: Prima is now a module
    * Qualcomm are insane and PRIMA doesn't really work when it's compiled in tree.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,6 +190,8 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # Inherit from the proprietary version
 -include vendor/bq/bardock/BoardConfigVendor.mk
