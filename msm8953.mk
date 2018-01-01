bb3b85b0c10e80061d6359192b201edfac389d5b
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    power: Switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Bug: 69270928
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- msm8953.mk
+++ msm8953.mk
@@ -411,14 +411,12 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@1.0 \
-    vendor.qti.hardware.perf@2.0
+    android.hardware.power@1.2-service.onclite-libperfmgr
 
 # Protobuf
 PRODUCT_PACKAGES += \
