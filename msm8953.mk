872e9d2ce73e05d9799ae38abc820b43f99d4ff2
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    lmkd: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- msm8953.mk
+++ msm8953.mk
@@ -346,7 +346,15 @@ PRODUCT_PACKAGES += \
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.log_stats=true
+    ro.lmk.log_stats=true \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100
 
 # Media
 PRODUCT_PACKAGES += \
