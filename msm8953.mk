08f4dad9e42a615de65d17e9c53f81a8215abdd5
Rajeev Kumar <rajekumar@google.com>
2018-03-12 15:26:29 -0700

    LMKD: Enable memory stats logging in user mode LMKD running on crosshatch devices.
    Bug:74443701
    Test: Tested manually
    Change-Id: I288098e17d5fc65923758152bba5e786a75edc4c

--- msm8953.mk
+++ msm8953.mk
@@ -298,6 +298,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# LMKD
+# Enable stats logging in LMKD
+TARGET_LMKD_STATS_LOG := true
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.log_stats=true
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert
