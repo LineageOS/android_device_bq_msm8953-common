b2e85d2278c8f00b88f444b118fd4002b1498f92
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-17 19:20:36 -0700

    bardock: Only build fingerprint@2.1 libs for prebuilt HAL

--- device.mk
+++ device.mk
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
