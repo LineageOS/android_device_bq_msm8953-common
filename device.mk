1a09adeb60abc18bfb0a16cb5f12305b006e2400
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-21 18:16:20 -0700

    bardock: Build netutils wrapper

--- device.mk
+++ device.mk
@@ -237,6 +237,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Network
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
