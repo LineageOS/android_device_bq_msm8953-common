aae40e8ee196c70b08c20fa74ea72325921f781f
Nilesh Shah <nsshah@codeaurora.org>
2018-04-12 17:08:20 +0530

    graphics: Update Vulkan version supported by GPU
    This code ensures that framework expose
    Vulkan 1.1 version
    
    Change-Id: I0227d138d398eb47b717bdef34921ec62e85292b

--- msm8953.mk
+++ msm8953.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
