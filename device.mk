ec8c0d9a05c27451f92e3b82ef2fdf5f9da9c856
Eloi Carracedo <eloimuns@gmail.com>
2018-05-15 21:04:23 +0200

    bardock: Build Snap

--- device.mk
+++ device.mk
@@ -134,7 +134,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
-    camera.device@3.2-impl
+    camera.device@3.2-impl \
+    Snap
 
 # mct_pipeline
 PRODUCT_COPY_FILES += \
