c8c1d5c5a65a82883db99eec2607c941bd720748
TheScarastic <warabhishek@gmail.com>
2018-07-03 00:42:30 +0530

    recovery: Add recovery scripts
     * Add bootdevice symlink
     * Advertise system_as_root
     * Support usb functions
    
    Change-Id: If04d1fb5aa7a80a0c806f6ca5d6b0872f6ec681a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,6 +42,22 @@ LOCAL_SRC_FILES    := init.qcom.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 # Common init scripts
 
 include $(CLEAR_VARS)
