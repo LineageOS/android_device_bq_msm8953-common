a163bca8bf2cbb18fafa591d5e41c6ee7d63dde7
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    overlay: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8953.mk
+++ msm8953.mk
@@ -18,8 +18,7 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # RRO (Runtime Resource Overlay)
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
