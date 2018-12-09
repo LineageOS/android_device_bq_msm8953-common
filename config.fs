1b17e4eaa2f2eb7384fe0b02783640c49a55891f
David <Davidteri91@gmail.com>
2018-12-09 22:57:50 +0100

    config.fs: add perms for imsdatadaemon and ims_rtp_daemon
    Change-Id: I2adad49ceda5f5bc4b882e385b2baecd522caed8
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- config.fs
+++ config.fs
@@ -115,6 +115,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
