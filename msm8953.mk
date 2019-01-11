977e2154854969e0371034446f76056d366ebd30
Quallenauge <Hamsi2k@freenet.de>
2019-01-11 20:05:08 +0100

    Update vendor security patch level
    * BQ's 2.5.1 blobs contain 2019 February patches.
    
    Change-Id: I0074a36e67f5948be7e6fe9950c231fc584a5937

--- msm8953.mk
+++ msm8953.mk
@@ -369,7 +369,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-09-05
+    ro.lineage.build.vendor_security_patch=2019-02-01
 
 # Vibrator
 PRODUCT_PACKAGES += \
