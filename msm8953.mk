0f403677cd6d7760aec38767dfb88bfde998973b
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    livedisplay: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- msm8953.mk
+++ msm8953.mk
@@ -259,6 +259,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.msm8953
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert
