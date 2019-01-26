2d365d969838954da643c8a97e7e7ab5d8e0b44d
Matthias Lenk <matthias.lenk@gmx.de>
2019-01-26 22:56:25 +0200

    vintf: Replace AOSP USB HAL with Lineage basic USB HAL
    Using the AOSP HAL, changing USB modes (such as MTP when connecting to
    PC) does not work through the "USB Preferences" settings fragment (but
    it does through Developer Options.
    
    Also, the AOSP HAL for O and P requires the /sys/class/dual_role_usb
    interface to be present and functional. For our 3.10 kernel, the FUSB302
    driver does not implement this interface.
    
    Using the Lineage basic HAL, MTP mode works properly. The HAL also
    reports canChangeDataRole = false and canChangePowerRole = false, which
    is currently adequate due to our limited FUSB302 kernel driver.
    
    Edit: This also applies to the bardock kernel (3.18),
          which doesn't support that feature.
    
    Change-Id: I52716a04e65ba446c9412dd3328df407d02d107a
    Signed-off-by: Matthias Lenk <matthias.lenk@gmx.de>
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -382,7 +382,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
