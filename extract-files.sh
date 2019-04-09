50b5d41025726c8aeced12f95b4c0de36ccbcbcf
Quallenauge <Hamsi2k@freenet.de>
2019-04-09 21:31:04 +0200

    proprietary-files: Update to latest pie blobs.
    * These where taken from sdm660 soc, which surprisingly
      work very stable (which points to a generic software design,
      which is used to support different socs).
    
    While at it perform the following changes:
    * Don't move ebms to vendor. They exists on all stock roms on /system.
    * Reorder adsp blobs and remove redundant values.
    * Reorder ims blobs.
    * Create link to audio.primary.msm8953.so for soundtrigger,
      which requests audio.primary.sdm660.so library.
    * !! RIL and QMI components can't be used and are sticked to the
      existing PIE blobs. Background: If using new blobs, the phone shows
      SIM cards connects to the mobile network,
      but it isn't possible to perform some phone calls due an immedately
      hangup from modem with no visible reason in logs.
    
    Change-Id: I385fbe3bf7106c3a39bc2609765ed2c00d121474

--- extract-files.sh
+++ extract-files.sh
@@ -64,9 +64,6 @@ function blob_fixup() {
         vendor/bin/mm-qcamera-app|vendor/lib/hw/camera.msm8953.so|vendor/lib/libmm-qcamera.so|vendor/lib/libmmcamera*|vendor/lib64/libmmcamera*)
                 sed -i -e 's|/data/misc/camera|/data/vendor/qcam|g' "${2}"
         ;;
-        vendor/etc/permissions/embms.xml)
-                sed -i -e 's|/system/framework|/vendor/framework|g' "${2}"
-        ;;
         vendor/etc/izat.conf)
                 sed -i -e 's|^GTP_CELL *=.*|GTP_CELL=BASIC|g' "${2}"
                 sed -i -e 's|^GTP_WIFI *=.*|GTP_WIFI=BASIC|g' "${2}"
