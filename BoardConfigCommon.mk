fcf9ed76cd9ad63476be8607d246f08bf8d019f2
Quallenauge <Hamsi2k@freenet.de>
2019-06-03 23:12:51 +0100

    BoardConfig: Disable pstore definition.
    It's now defined directly within the dts.
    
    Change-Id: I3569e5841c10e80e906c8d0a4551d1177c195043

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,7 +133,6 @@ TARGET_PROVIDES_KEYMASTER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
-BOARD_KERNEL_CMDLINE += ramoops.mem_address=0x30000000 ramoops.mem_size=0x300000 ramoops.ecc=1 ramoops.console_size=0x20000
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
