206da0524ebc9081f89b3d735b5903b46eb24f53
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-09 16:52:33 +0200

    build: Move soong namespaces to device.mk
    * This is a product property
    
    Change-Id: I411428687839fca7c794d488bb48e5f1773e570b

--- msm8953.mk
+++ msm8953.mk
@@ -403,6 +403,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
