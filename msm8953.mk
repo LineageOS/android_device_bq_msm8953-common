29f5ccf4d22f96f6c8865732b823b610814aba9f
Davide Garberi <dade.garberi@gmail.com>
2019-09-15 17:25:18 +0200

    thermal: Switch to thermal 2.0
    Change-Id: I06e67a3c3ab10a64362d7a4b95c3d256bcd39798

--- msm8953.mk
+++ msm8953.mk
@@ -404,6 +404,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.mock
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
