85d14b2b21db9acbee71183da1048a63121f1597
Quallenauge <Hamsi2k@freenet.de>
2019-11-05 22:18:43 +0200

    thermal: Build thermal modules.
    Change-Id: I690f4eb85b2376535b0d5d81a5926d591f5267c2

--- msm8953.mk
+++ msm8953.mk
@@ -462,6 +462,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8953
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
