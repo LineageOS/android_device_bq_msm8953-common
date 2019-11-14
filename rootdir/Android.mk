e751d4823096150a87068a712d79f4e2a2ccb76c
Quallenauge <Hamsi2k@freenet.de>
2019-11-14 20:27:48 +0100

    init: Add init.class_main.sh from LA.UM.8.6.2.r1-04900-89xx.0.
    Change-Id: I57c43dd27642490730c3fa96e2490c1bb61e32ca

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,6 +19,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.class_main.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.class_main.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
