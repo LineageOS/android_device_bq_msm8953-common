e5c305e34ceec0198fc6cbf9ed598dd40369d57f
Quallenauge <Hamsi2k@freenet.de>
2020-02-20 20:29:02 +0100

    fstman: Remove fstman.
    * WIFI bonding not needed on this device.
    
    Change-Id: Ic38aba81873198ea3e3d5085ee73d2a9bf5f9333

--- msm8953.mk
+++ msm8953.mk
@@ -520,7 +520,6 @@ PRODUCT_PACKAGES += \
 
 # Wi-FI
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/hostapd_default.conf:$(TARGET_OUT_VENDOR)/etc/hostapd/hostapd_default.conf \
