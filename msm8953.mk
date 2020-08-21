5ec4d9d84c7157028ad7b8f85344e50d490cb5ba
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    tether: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- msm8953.mk
+++ msm8953.mk
@@ -483,6 +483,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
