7f1af3fbec203f357382fbf2d902a44d0634c6be
Quallenauge <Hamsi2k@freenet.de>
2020-09-09 22:19:01 +0200

    proprietary-files: Switch to fairphone Q release blobs.
    Source: FP3-user-10-8901.3.A.0033.20200815-08150109-release-keys
    
    Left sensor blobs out, due problems with proximity sensor,
    which stalls after some usage.
    
    Change-Id: Iec4bd3357011e16906dddbecdf2bb1f522b14e38

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,7 @@ fi
 
 function blob_fixup() {
         case "${1}" in
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+        system/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|system/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
                 sed -i -e 's|xml version=\"2.0\"|xml version=\"1.0\"|g' "${2}"
         ;;
         lib64/libwfdnative.so)
