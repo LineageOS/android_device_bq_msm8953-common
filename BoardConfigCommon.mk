3b05d3c903acb848123cf3e888d9ea9db1121a5b
Quallenauge <Hamsi2k@freenet.de>
2020-10-02 20:15:44 +0200

    [DNM] Set permissive mode.
    Change-Id: I0cbfdbc5ed41100fa6f5244888e03ee9236e7983

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,6 +136,7 @@ TARGET_PROVIDES_KEYMASTER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM,115200,n8 androidboot.console=ttyMSM androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
