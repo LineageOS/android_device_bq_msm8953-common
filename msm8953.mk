07c3087857833ef54cac249494c0a83837412411
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    health: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- msm8953.mk
+++ msm8953.mk
@@ -293,8 +293,8 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
