09e2e378b11bb8e7e3537b2a5a18d378abe77db0
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 20:43:12 +0200

    extract-utils: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,59 +1,60 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-DEVICE=$1
-VENDOR=bq
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-# Load extractutils and do some sanity checks
-MY_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
-LINEAGE_ROOT=$(readlink -f "${MY_DIR}/../../..")
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at $HELPER"
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "${HELPER}"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
-KANG=
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -p | --path )           shift
-                                SRC=$1
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        -k | --kang)            KANG="--kang"
-                                ;;
-        -n|--no-cleanup)        CLEAN_VENDOR=false
-                                ;;
+ONLY_COMMON=
+ONLY_TARGET=
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-common )
+                ONLY_COMMON=true
+                ;;
+        --only-target )
+                ONLY_TARGET=true
+                ;;
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 function blob_fixup() {
@@ -123,16 +124,22 @@ function blob_fixup() {
                 sed -i -e 's|android.hardware.radio.config@1.1::IRadioConfig\x00|android.hardware.radio.config@1.0::IRadioConfig\x00|g' "${2}"
         esac
 }
-# Reinitialize the helper for ${device}
-(
-        source "${DEVICE}/extract-files.sh"
-        setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
-        extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-                        ${KANG} --section "${SECTION}"
-        if [ -s "${MY_DIR}/proprietary-files-twrp.txt" ]; then
-                extract "${MY_DIR}/proprietary-files-twrp.txt" "$SRC" \
-                        ${KANG} --section "${SECTION}"
-        fi
-)
 
-"${MY_DIR}/setup-makefiles.sh" "${CLEAN_VENDOR}"
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    source "${MY_DIR}/../${DEVICE}/extract-files.sh"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+COMMON_BLOB_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"/"${DEVICE_COMMON}"/proprietary
+
+"${MY_DIR}"/setup-makefiles.sh
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,56 +1,47 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Load extractutils and do some sanity checks
-MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
-CLEANUP="$1"
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
-	echo "Unable to find helper script at $HELPER"
-	exit 1
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
 fi
-. "${HELPER}"
+source "${HELPER}"
 
-# Reinitialize the helper for ${device}
-(
-	setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" "false" "${CLEANUP}"
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-	# Copyright headers and guards
-	write_headers
+# Warning headers and guards
+write_headers "bardock"
 
-	# $1: The device-specific blobs
-	# $2: Make treble compatible paths and put "$(TARGET_COPY_OUT_VENDOR)"
-	#     in generated makefiles
-	write_makefiles "${MY_DIR}/proprietary-files.txt" true
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-	if [ -f "${MY_DIR}/proprietary-files-twrp.txt" ]; then
-		cat >> "${BOARDMK}" <<-EOF
-			ifeq (\$(WITH_TWRP),true)
-			TARGET_RECOVERY_DEVICE_DIRS += vendor/${VENDOR}/${DEVICE}/proprietary
-			endif
-		EOF
-	fi
+# Finish
+write_footers
 
-	# We are done!
-	write_footers
-)
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+
+    # Warning headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+
+    # Finish
+    write_footers
+fi
