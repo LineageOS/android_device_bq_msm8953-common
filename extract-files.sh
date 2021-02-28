e1bcbab00a8bff56451b193107355382726d666d
Quallenauge <Hamsi2k@freenet.de>
2021-02-28 19:40:28 +0100

    proprietary-files: Set GTP_MODE to SDK_WIFI.
    Before change Ied2f14f6d1d01b6692cfedb69cd2a9ea18958e5c
    (New GTP_MODE in izat.conf to simplify the setup of GTP)
    and subsequent ones, the
    GTP_CELL
    GTP_WIFI
    (both are set to BASIC in stock) are changed to
    GTP_MODE. The value "SDK_WIFI" corresponds to the former values.
    
    Change-Id: I278c88cf5e109b5c8afab9b8addde596c9774f7e

--- extract-files.sh
+++ extract-files.sh
@@ -95,8 +95,7 @@ function blob_fixup() {
                 sed -i -e 's|/data/misc/camera|/data/vendor/qcam|g' "${2}"
         ;;
         vendor/etc/izat.conf)
-                sed -i -e 's|^GTP_CELL *=.*|GTP_CELL=BASIC|g' "${2}"
-                sed -i -e 's|^GTP_WIFI *=.*|GTP_WIFI=BASIC|g' "${2}"
+                sed -i -e 's|^GTP_MODE *=.*|GTP_MODE=SDK_WIFI|g' "${2}"
                 sed -i -e 's|^NLP_MODE *=.*|NLP_MODE=4|g' "${2}"
         ;;
         vendor/bin/netmgrd)
