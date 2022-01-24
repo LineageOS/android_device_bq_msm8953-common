0e1c0f64b7928f65ae6efc23e3aea1140b9d0d07
Quallenauge <Hamsi2k@freenet.de>
2022-01-24 19:56:58 +0100

    camera: Adapt DisplayEventReceiver shim to A12.
    Change-Id: I44712965b5d1b15ceb07044ede4d0e4bf608e668

--- camera/DisplayEventReceiver.cpp
+++ camera/DisplayEventReceiver.cpp
@@ -2,9 +2,10 @@
 #include <gui/ISurfaceComposer.h>
 
 namespace android {
-    extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceENS1_13ConfigChangedE(void* vsyncSource, ISurfaceComposer::ConfigChanged configChanged);
+    extern "C" void _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_5FlagsINS1_17EventRegistrationEEE(void* vsyncSource, ISurfaceComposer::EventRegistrationFlags eventRegistration);
 
     extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE(void *vsyncSource) {
-                    _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceENS1_13ConfigChangedE(vsyncSource, ISurfaceComposer::eConfigChangedSuppress);
+                    ISurfaceComposer::EventRegistrationFlags eventRegistration = {};
+                    _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_5FlagsINS1_17EventRegistrationEEE(vsyncSource, eventRegistration);
     }
 }
