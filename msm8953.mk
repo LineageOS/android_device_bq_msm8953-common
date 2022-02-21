d1199463887ff80938f0c0771747bbf51193f7fb
Quallenauge <Hamsi2k@freenet.de>
2022-02-21 20:24:00 +0100

    props: Move dalvik properties to specific device tree.
    Change-Id: I650bf52c2e43a3804fd7c5336c14e0807111e8dc

--- msm8953.mk
+++ msm8953.mk
@@ -213,14 +213,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
