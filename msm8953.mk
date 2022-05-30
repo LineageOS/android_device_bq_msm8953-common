ced48f9471a236365804e5fca82132ae02d75127
Quallenauge <Hamsi2k@freenet.de>
2022-05-30 20:24:00 +0100

    camera: Cleanup camera providers.
    Change-Id: I3d3468cae11bec80e148310ab234f43a5d9334b7

--- msm8953.mk
+++ msm8953.mk
@@ -168,12 +168,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     Gallery2 \
