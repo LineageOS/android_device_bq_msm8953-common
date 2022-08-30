57481feb22899f0a61c967177d151c4f2d404ce2
Quallenauge <Hamsi2k@freenet.de>
2022-08-30 19:46:11 +0100

    props: Remove all lmkd custom settings.
    Default values are working better
    for this device.
    
    Change-Id: I929667b0e3ef71ae6a39ade88b59f388ff05564c

--- msm8953.mk
+++ msm8953.mk
@@ -345,21 +345,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
-# LMKD
-# Enable stats logging in LMKD
-TARGET_LMKD_STATS_LOG := true
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.log_stats=true \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert
