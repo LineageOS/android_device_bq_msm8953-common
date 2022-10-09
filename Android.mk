9902d5e5683bcfb012389f564c989fc81398a796
Michael Bestas <mkbestas@lineageos.org>
2022-10-09 21:19:55 +0200

    ims: Move telephony related packages to /system_ext
    Change-Id: I6819c2af9f0f6e0e3823b853277287fc5aa8d504

--- Android.mk
+++ Android.mk
@@ -50,12 +50,12 @@ ALL_DEFAULT_INSTALLED_MODULES += $(QCRIL_SYMLINKS)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
