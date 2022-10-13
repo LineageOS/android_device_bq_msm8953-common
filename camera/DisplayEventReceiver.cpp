ea85219c99e3fe9011044e2e264f9d914ca2ed11
jmpfbmx <jmpf.bmx@gmail.com>
2022-10-13 19:52:55 +0200

    msm8953-common: camera: Adapt DisplayEventReceiver shim to A13
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I74e82ce8ee38e8024ffabb99ca548d7812bbf92d

--- camera/DisplayEventReceiver.cpp
+++ camera/DisplayEventReceiver.cpp
@@ -2,10 +2,10 @@
 #include <gui/ISurfaceComposer.h>
 
 namespace android {
-    extern "C" void _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_5FlagsINS1_17EventRegistrationEEE(void* vsyncSource, ISurfaceComposer::EventRegistrationFlags eventRegistration);
+    extern "C" void _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_3ftl5FlagsINS1_17EventRegistrationEEE(void* vsyncSource, ISurfaceComposer::EventRegistrationFlags eventRegistration);
 
     extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE(void *vsyncSource) {
                     ISurfaceComposer::EventRegistrationFlags eventRegistration = {};
-                    _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_5FlagsINS1_17EventRegistrationEEE(vsyncSource, eventRegistration);
+                    _ZN7android20DisplayEventReceiverC2ENS_16ISurfaceComposer11VsyncSourceENS_3ftl5FlagsINS1_17EventRegistrationEEE(vsyncSource, eventRegistration);
     }
 }
