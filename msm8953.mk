e6866ad0a2e22003c841e51259692cac58b8fbbb
Bruno Martins <bgcngm@gmail.com>
2022-10-13 18:48:56 +0200

    props: Split system, system_ext, odm, product, and vendor props
    * system_ext.prop, system.prop created to adress
      SELinux permission check failed on some props
    
    Change-Id: Iee726d7d95d2bc4abb14fcfdffc55a04aca6e1cc

--- msm8953.mk
+++ msm8953.mk
@@ -160,9 +160,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
-# Bpf
-PRODUCT_PROPERTY_OVERRIDES += ro.kernel.ebpf.supported=1
-
 # Camera
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v28.so \
@@ -187,21 +184,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mct_pipeline.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mct_pipeline.xml
 
-
-# Dalvik/HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
 # Disable skip validate
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1
@@ -213,11 +195,6 @@ PRODUCT_PACKAGES += \
 # Use threaded GLES render engine
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.renderengine.backend=threaded
-
-# Skia
-# (b/183612348): Enable skia reduceOpsTaskSplitting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    renderthread.skia.reduceopstasksplitting=true
 
 # ScalarConfig
 PRODUCT_PROPERTY_OVERRIDES += \
