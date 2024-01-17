790a2c1ce5bbca86c8eafc3a81084ab3d25f4ab5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-17 08:40:59 +0100

    msm8953-common: Switch to two-stage init mounting
    * Stop handling vendor mounting from kernel, first stage init is gonna
      do that, together with system (which is now /system)
    * Use correct mount flags coming from kernel dts
    * Move system and vendor mount points to /dev/block/by-name/, we don't
      have bootdevice symlink to that point in init
    * Copy fstab to recovery for first stage init
    
    Change-Id: Iaa0ecc3f13c3cfd3fe357ca509549d0f68a29cf0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,7 +39,17 @@ LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
