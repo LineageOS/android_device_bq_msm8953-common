614aa6d0bab2822647ab77618db5d8aea8b5b2bd
jmpfbmx <jmpf.bmx@gmail.com>
2024-01-17 20:07:41 +0100

    msm8953-common: Build health@2.1 vendor
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I9f07d631c015b08a725358c9eb922a4db0227863

--- msm8953.mk
+++ msm8953.mk
@@ -274,6 +274,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.1.vendor \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
